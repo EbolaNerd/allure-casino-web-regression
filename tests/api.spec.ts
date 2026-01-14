62ad2fc1b4feb0fab73c9cb8693c6e022a678e22
Lars Wolter <lwolter1337@gmail.com>
2026-01-14 15:39:54 +0100

    Grouped tests in describe blocks

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,36 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-test('Search games & providers', async ({ request }) => {
-  const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/search', {params: {query: 'Play'}})
+test.describe(('Casino app API services'), () => {
+  test('Search games & providers', async ({ request }) => {
+    const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/search', {params: {query: 'Play'}})
 
-  // Expect status code is 200
-  await expect(response.status()).toBe(200);
-});
+    // Expect status code is 200
+    await expect(response.status()).toBe(200);
+  });
 
-test('Search games (Depricated)', async ({ request }) => {
-  const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/games', {params: {name: 'Play'}})
+  test('Search games (Depricated)', async ({ request }) => {
+    const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/games', {params: {name: 'Play'}})
 
-  // Expect status code is 200
-  await expect(response.status()).toBe(200);
-});
+    // Expect status code is 200
+    await expect(response.status()).toBe(200);
+  });
 
-test('Get Jackpot', async ({ request }) => {
-  const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/jackpot/gold_jackpot')
+  test('Get Jackpot', async ({ request }) => {
+    const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/v1/jackpot/gold_jackpot')
 
-  // Expect status code is 200
-  await expect(response.status()).toBe(200);
-});
+    // Expect status code is 200
+    await expect(response.status()).toBe(200);
+  });
 
-test('GET Campaigns', async ({ request }) => {
-  const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/content/v1/campaigns')
+  test('GET Campaigns', async ({ request }) => {
+    const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/content/v1/campaigns')
 
-  // Expect status code is 200
-  await expect(response.status()).toBe(200);
-});
+    // Expect status code is 200
+    await expect(response.status()).toBe(200);
+  });
 
-test('Get Lobby', async ({ request }) => {
-  const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/lobby/v1?type=Casino', {params: {type: 'Casino'}})
+  test('Get Lobby', async ({ request }) => {
+    const response = await request.get('https://casino-app-gateway.ds-api.dk/api/casino/game/lobby/v1?type=Casino', {params: {type: 'Casino'}})
 
-  // Expect status code is 200
-  await expect(response.status()).toBe(200);
-});
+    // Expect status code is 200
+    await expect(response.status()).toBe(200);
+  });
+})
+
+
